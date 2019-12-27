53f5f86211f4355448a5006f2e944c3dfcf32483
MayronSouza <mayrondrummer@gmail.com>
2019-12-27 20:17:42 -0300

    Criando SpotController e importando multer

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,10 @@ const express = require('express')
 const routes = express.Router()
 
 const SessionController = require('./controllers/SessionController')
+const SpotController = require('./controllers/SpotController')
 
 routes.post('/sessions', SessionController.store)
+
+routes.post('/spots', SpotController.store)
 
 module.exports = routes
